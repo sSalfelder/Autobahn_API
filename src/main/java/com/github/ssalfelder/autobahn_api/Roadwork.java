beff218a57cf9db2fa551d1e7dc0c6e05d529a17
sSalfelder <sascha.salfelder@gmx.de>
2025-06-25 10:58:12 +0200

    creadted Roadwork class

--- src/main/java/com/github/ssalfelder/autobahn_api/Roadwork.java
+++ src/main/java/com/github/ssalfelder/autobahn_api/Roadwork.java
@@ -1,4 +1,59 @@
 package com.github.ssalfelder.autobahn_api;
 
 public class Roadwork {
+
+    private String road;
+    private String section;
+    private String direction;
+    private String start;
+    private String end;
+
+    public String getRoad() {
+        return road;
+    }
+
+    public void setRoad(String road) {
+        this.road = road;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public Roadwork(String road, String section, String direction, String start, String end) {
+        this.road = road;
+        this.section = section;
+        this.direction = direction;
+        this.start = start;
+        this.end = end;
+    }
+
 }
