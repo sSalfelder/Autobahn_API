fbb9bbc0c31f7b8be9a7af3ecffb9e4ac7900985
sSalfelder <sascha.salfelder@gmx.de>
2025-06-25 17:54:36 +0200

    added TextArea for the parsed response text

--- src/main/java/com/github/ssalfelder/autobahn_api/Controller.java
+++ src/main/java/com/github/ssalfelder/autobahn_api/Controller.java
@@ -3,18 +3,30 @@ package com.github.ssalfelder.autobahn_api;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     private Button highwayButton;
 
+    @FXML
+    private TextArea outputArea;
+
     private ObservableList<Roadwork> roadworkList  = FXCollections.observableArrayList();
     private final String URL = "https://verkehr.autobahn.de/o/autobahn/A1/services/roadworks";
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        outputArea.setVisible(false);
+    }
 
     public Node getDragNode() {
         return highwayButton;
@@ -22,23 +34,31 @@ public class Controller {
 
     @FXML
     protected void onHighwayClicked() {
-        APIRequest request = new APIRequest();
-        request.getData(URL, this::handleRoadworkData);
+        if (!outputArea.isVisible()) {
+            APIRequest request = new APIRequest();
+            outputArea.setVisible(true);
+            request.getData(URL, this::handleRoadworkData);
+        } else {
+            outputArea.setVisible(false);
+        }
     }
 
     private void handleRoadworkData(ArrayList<Roadwork> data){
         roadworkList.clear();
         roadworkList.addAll(data);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Baustellen: \n\n");
 
         for (Roadwork rw : roadworkList) {
-            System.out.println("-------------------------------------------");
-            System.out.printf("%s ", rw.getSection());
-            System.out.printf("%s ", rw.getDirection());
-            System.out.printf("%s ", rw.getStart());
-            System.out.printf("%s %n", rw.getEnd());
-            System.out.println("-------------------------------------------");
+            sb.append("Abschnitt: " + rw.getSection() + "\n");
+            sb.append("Richtung: " + rw.getDirection() + "\n");
+            sb.append(rw.getStart() + "\n");
+            sb.append(rw.getEnd() + "\n\n");
         }
 
+        String result = sb.toString();
+        outputArea.setText(result);
     }
+
 
 }
